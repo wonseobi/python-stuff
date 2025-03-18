f5fbe1188be761527a9dfd49a5979d67c8cb170f
Won Lee <wonseob2207@gmail.com>
2025-03-18 12:09:32 -0600

    Add files via upload

---
+++ myproject.py
@@ -0,0 +1,26 @@
+# Project
+# Calculates in % my progress for six months in the gym ;D
+from time import sleep
+progress = 23.24
+print("Hello Won, today's date: 18/3/25")
+answer = input("Did you assist today? (Y/N) ").upper()
+
+if answer == "Y":
+    print("Well done!")
+    sleep(1)
+    print("Updating progress...")
+    sleep(3)
+    progress = progress + 0.83
+    progress = round(progress, 2)
+    print(f"Current progress {progress}%")
+    sleep(1)
+    print("Remember to add a new pin!")
+    sleep(1)
+    print("See you tomorrow :)")
+elif answer == "N":
+    print("Please stay on track")
+    sleep(1)
+    print("Missing once is a mistake, missing twice is the start of a new habit")
+    sleep(3)
+    print("See you tomorrow :)")
+
